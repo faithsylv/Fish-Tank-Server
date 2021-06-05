099f6f77f2bc0d801a666cac82c291ee7b8541e9
faithsylvia1 <you@example.com>
2021-06-05 16:51:10 +1000

    refresh token route working

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,10 @@ const express = require("express")
 const router = express.Router()
 const User = require("../models/user")
 const passport = require("passport")
+const jwt = require("jsonwebtoken")
 
 const { getToken, COOKIE_OPTIONS, getRefreshToken } = require("../authenticate")
+
 router.post("/signup", (req, res, next) => {
   // Verify that first name is not empty
   if (!req.body.firstName) {
@@ -59,6 +61,55 @@ router.post("/login", passport.authenticate("local"), (req, res, next) => {
     },
     err => next(err)
   )
+})
+
+router.post("/refreshToken", (req, res, next) => {
+  const { signedCookies = {} } = req
+  const { refreshToken } = signedCookies
+  if (refreshToken) {
+    try {
+      const payload = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET)
+      const userId = payload._id
+      User.findOne({ _id: userId }).then(
+        user => {
+          if (user) {
+            // Find the refresh token against the user record in database
+            const tokenIndex = user.refreshToken.findIndex(
+              item => item.refreshToken === refreshToken
+            )
+            if (tokenIndex === -1) {
+              res.statusCode = 401
+              res.send("Unauthorized")
+            } else {
+              const token = getToken({ _id: userId })
+              // If the refresh token exists, then create new one and replace it.
+              const newRefreshToken = getRefreshToken({ _id: userId })
+              user.refreshToken[tokenIndex] = { refreshToken: newRefreshToken }
+              user.save((err, user) => {
+                if (err) {
+                  res.statusCode = 500
+                  res.send(err)
+                } else {
+                  res.cookie("refreshToken", newRefreshToken, COOKIE_OPTIONS)
+                  res.send({ success: true, token })
+                }
+              })
+            }
+          } else {
+            res.statusCode = 401
+            res.send("Unauthorized")
+          }
+        },
+        err => next(err)
+      )
+    } catch (err) {
+      res.statusCode = 401
+      res.send("Unauthorized")
+    }
+  } else {
+    res.statusCode = 401
+    res.send("Unauthorized")
+  }
 })
 
 module.exports = router
